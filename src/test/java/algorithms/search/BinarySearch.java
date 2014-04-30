a9a2b4235e4ccb573525f6840ed16d9aaa211731
Alexey Plotnik <odiszapc@gmail.com>
2014-04-30 18:20:45 +1100

    binary search

--- src/test/java/algorithms/search/BinarySearch.java
+++ src/test/java/algorithms/search/BinarySearch.java
@@ -34,12 +34,18 @@ public class BinarySearch {
     }
 
     @Test
+    public void binarySearchSingleElement() throws Exception {
+        int[] array = {13};
+        int index = findFirstIndex(array, 13);
+        Assert.assertEquals(index, 0);
+    }
+
+    @Test
     public void binarySearchOddNotFound() throws Exception {
         int[] array = {2, 14, 19, 43, 50, 84, 94, 100};
         int index = findFirstIndex(array, 13);
         Assert.assertEquals(index, -1);
     }
-
 
     private int findFirstIndex(int[] array, int value) {
         int index = -1;
@@ -50,11 +56,11 @@ public class BinarySearch {
     }
 
     private int findFirstIndex(int[] array, int value, int from, int to) {
-        if (from == to)
-            return -1;
-
         if (array[from] == value)
             return from;
+
+        if (from == to)
+            return -1;
 
         int middle = (to - from) / 2;
         if (value >= array[middle])
