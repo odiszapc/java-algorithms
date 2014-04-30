7c72f1b514e22aa46ed68d1c48bb691c1ac55eb0
Alexey Plotnik <odiszapc@gmail.com>
2014-04-30 18:00:26 +1100

    binary search

---
+++ src/test/java/algorithms/search/BinarySearch.java
@@ -0,0 +1,40 @@
+package algorithms.search;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class BinarySearch {
+
+    @Test
+    public void binarySearchEven() throws Exception {
+        int[] array = {2, 14, 19, 43, 50, 84, 94};
+        int index = findFirstIndex(array, 14);
+        Assert.assertEquals(index, 1);
+    }
+
+    @Test
+    public void binarySearchOdd() throws Exception {
+        int[] array = {2, 14, 19, 43, 50, 84, 94, 100};
+        int index = findFirstIndex(array, 14);
+        Assert.assertEquals(index, 1);
+    }
+
+    private int findFirstIndex(int[] array, int value) {
+        int index = -1;
+        if (0 == array.length)
+            return index;
+
+        return findFirstIndex(array, value, 0, array.length - 1);
+    }
+
+    private int findFirstIndex(int[] array, int value, int from, int to) {
+        if (array[from] == value)
+            return from;
+
+        int middle = (to - from) / 2;
+        if (value >= array[middle])
+            return findFirstIndex(array, value, from + middle, from + middle + (to - from - middle));
+        else
+            return findFirstIndex(array, value, from, middle - (1 - (to - from) % 2));
+    }
+}
