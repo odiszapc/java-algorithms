0802245b9782a3cd7d997ca0dcc99ca76bb5cb1f
Alexey Plotnik <odiszapc@gmail.com>
2014-05-02 21:26:38 +1100

    Add tests of sort

--- src/test/java/algorithms/sort/SortTestBase.java
+++ src/test/java/algorithms/sort/SortTestBase.java
@@ -9,6 +9,18 @@ public abstract class SortTestBase {
     private static final int[] ARRAY_ASC = {2, 14, 19, 43, 50, 84, 94};
     private static final int[] ARRAY_DESC = {94, 84, 50, 43, 19, 14, 2};
 
+    private static final int[] ARRAY_EMPTY = {};
+    private static final int[] ARRAY_EMPTY_ASC = {};
+    private static final int[] ARRAY_EMPTY_DESC = {};
+
+    private static final int[] ARRAY_SINGLE = {14};
+    private static final int[] ARRAY_SINGLE_ASC = {14};
+    private static final int[] ARRAY_SINGLE_DESC = {14};
+
+    private static final int[] ARRAY_MINIMAL = {14, 2};
+    private static final int[] ARRAY_MINIMAL_ASC = {2, 14};
+    private static final int[] ARRAY_MINIMAL_DESC = {14, 2};
+
     protected abstract void sortAsc(int[] array);
 
     protected abstract void sortDesc(int[] array);
@@ -20,8 +32,44 @@ public abstract class SortTestBase {
     }
 
     @Test
+    public void sortEmptyAscTest() throws Exception {
+        sortAsc(ARRAY_EMPTY);
+        Assert.assertArrayEquals(ARRAY_EMPTY_ASC, ARRAY_EMPTY);
+    }
+
+    @Test
+    public void sortSingleAscTest() throws Exception {
+        sortAsc(ARRAY_SINGLE);
+        Assert.assertArrayEquals(ARRAY_SINGLE_ASC, ARRAY_SINGLE);
+    }
+
+    @Test
+    public void sortMinimalAscTest() throws Exception {
+        sortAsc(ARRAY_MINIMAL);
+        Assert.assertArrayEquals(ARRAY_MINIMAL_ASC, ARRAY_MINIMAL);
+    }
+
+    @Test
     public void sortDescTest() throws Exception {
         sortDesc(ARRAY);
         Assert.assertArrayEquals(ARRAY_DESC, ARRAY);
+    }
+
+    @Test
+    public void sortEmptyDescTest() throws Exception {
+        sortDesc(ARRAY_EMPTY);
+        Assert.assertArrayEquals(ARRAY_EMPTY_DESC, ARRAY_EMPTY);
+    }
+
+    @Test
+    public void sortSingleDecTest() throws Exception {
+        sortDesc(ARRAY_SINGLE);
+        Assert.assertArrayEquals(ARRAY_SINGLE_DESC, ARRAY_SINGLE);
+    }
+
+    @Test
+    public void sortMinimalDescTest() throws Exception {
+        sortDesc(ARRAY_MINIMAL);
+        Assert.assertArrayEquals(ARRAY_MINIMAL_DESC, ARRAY_MINIMAL);
     }
 }
