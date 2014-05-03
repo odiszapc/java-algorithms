ebaf403bc9efa415ff456b833ada0f910c26cb44
Alexey Plotnik <odiszapc@gmail.com>
2014-05-03 13:56:21 +1100

    Add swap without memory overhead

--- src/test/java/algorithms/numbers/Swap.java
+++ src/test/java/algorithms/numbers/Swap.java
@@ -6,13 +6,26 @@ import org.junit.Test;
 public class Swap {
 
     @Test
-    public void swap() throws Exception {
+    public void slowSwap() throws Exception {
         int a = 111;
         int b = 222;
 
         int tmp = a;
         a = b;
         b = tmp;
+
+        Assert.assertEquals(222, a);
+        Assert.assertEquals(111, b);
+    }
+
+    @Test
+    public void fastSwap() throws Exception {
+        int a = 111;
+        int b = 222;
+
+        a ^= b;
+        b ^= a;
+        a ^= b;
 
         Assert.assertEquals(222, a);
         Assert.assertEquals(111, b);
