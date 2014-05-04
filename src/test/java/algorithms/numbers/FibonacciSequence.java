271d09ee841695959f453f903959e15d4f0b8657
Alexey Plotnik <odiszapc@gmail.com>
2014-05-04 23:40:11 +1100

    add recursive version of fibonacci sequence generator

--- src/test/java/algorithms/numbers/FibonacciSequence.java
+++ src/test/java/algorithms/numbers/FibonacciSequence.java
@@ -7,16 +7,25 @@ public class FibonacciSequence {
 
     @Test
     public void fibonacci() throws Exception {
-        Assert.assertEquals(55, fibonacci(11));
+        Assert.assertEquals(55, fib(10));
     }
 
-    private int fibonacci(int x) {
-        int[] row = new int[x];
+    @Test
+    public void fibonacciRecursive() throws Exception {
+        Assert.assertEquals(55, fibRec(10));
+    }
+
+    private int fibRec(int x) {
+        return x < 2 ? x : (fibRec(x - 2) + fibRec(x - 1));
+    }
+
+    private int fib(int x) {
+        int[] row = new int[x+1];
         row[0] = 0;
         row[1] = 1;
         for (int i = 2; i < row.length; i++)
             row[i] = row[i-2] + row[i-1];
 
-        return row[x-1];
+        return row[x];
     }
 }
