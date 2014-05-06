0dac499ab36eb6a026629899db4c8467d3c8611d
Alexey Plotnik <odiszapc@gmail.com>
2014-05-06 22:23:57 +1100

    optimize for loop

--- src/test/java/algorithms/arrays/ArrayBasics.java
+++ src/test/java/algorithms/arrays/ArrayBasics.java
@@ -10,9 +10,9 @@ public class ArrayBasics {
         int[] array = {94, 19, 50, 14, 2, 43, 84};
 
         int max = array[0];
-        for (int i = 0; i < array.length; i++)
-            if (array[i] > max)
-                max = array[i];
+        for (int anArray : array)
+            if (anArray > max)
+                max = anArray;
 
         Assert.assertEquals(94, max);
     }
