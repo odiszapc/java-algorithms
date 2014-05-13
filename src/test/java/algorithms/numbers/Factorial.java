f937fe19a626714fba6c94b2488ded55b9856fed
Alexey Plotnik <odiszapc@gmail.com>
2014-05-13 17:56:13 +1100

    add tail recursive version of factorial algorithm

--- src/test/java/algorithms/numbers/Factorial.java
+++ src/test/java/algorithms/numbers/Factorial.java
@@ -11,9 +11,25 @@ public class Factorial {
         Assert.assertEquals(24, factorial(4));
     }
 
+    @Test
+    public void factorialTailRecursive() throws Exception {
+        Assert.assertEquals(24, factorialTailRecursive(4));
+    }
+
+
     private int factorial(int x) {
         if (0 == x)
             return 1;
         return x * factorial(x - 1);
+    }
+
+    private int factorialTailRecursive(int x) {
+        return factorialTailRecursive(x, 1);
+    }
+
+    private int factorialTailRecursive(int x, int acc) {
+        if (0 == x)
+            return acc;
+        return factorialTailRecursive(x - 1, acc * x);
     }
 }
